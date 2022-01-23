f9a0d6d2a63f50a5169eabb9720233cdbc51d3bd
Petr Ruzicka <petr.ruzicka@gmail.com>
2022-01-23 09:55:45 +0100

    Add initial Terraform code

--- clusters/dev-sandbox/ruzickap-eks.k8s.use1.dev.proj.aws.mylabs.dev/cluster-variables.tfvars
+++ clusters/dev-sandbox/ruzickap-eks.k8s.use1.dev.proj.aws.mylabs.dev/cluster-variables.tfvars
@@ -1,2 +1,16 @@
 cluster_name = "ruzickap-eks"
 cluster_fqdn = "ruzickap-eks.test.k8s.mylabs.dev"
+
+aws_vpc_cidr        = "10.0.0.0/21"
+aws_private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+aws_public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+aws_default_tags = {
+  cluster_group       = "dev-sandbox"
+  owner               = "petr.ruzicka@gmail.com"
+  entity              = "org1"
+  environment         = "dev"
+  data-classification = "green"
+  product_id          = "12345"
+  department          = "myit"
+  charge-code         = "4321"
+}
--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,3 +1,32 @@
 terraform {
   backend "s3" {}
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.73.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.7.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.4.1"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
+locals {
+  vpc_name = var.cluster_fqdn
+}
+
+provider "aws" {
+  # specify default tags that will be applied to ALL resources: https://www.hashicorp.com/blog/default-tags-in-the-terraform-aws-provider
+  default_tags {
+    tags = var.aws_default_tags
+  }
+
+  region = var.aws_default_region
 }
