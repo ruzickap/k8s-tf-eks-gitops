4eef4c80a05accb1b42ee3242a70c7a7fffa6a73
Petr Ruzicka <petr.ruzicka@gmail.com>
2022-02-19 18:13:15 +0100

    Add Amazon EKS to Terraform

--- clusters/dev-sandbox/group-variables.tfvars
+++ clusters/dev-sandbox/group-variables.tfvars
@@ -2,6 +2,9 @@ aws_default_region                       = "eu-central-1"
 aws_github_oidc_federated_role_to_assume = "arn:aws:iam::123456789012:role/GitHubOidcFederatedRole"
 terraform_code_dir                       = "terraform/dev"
 cluster_version                          = "1.21"
+cloudwatch_log_group_retention_in_days   = "1"
+cluster_enabled_log_types                = [] # "audit", "authenticator"
+
 aws_tags_group_level = {
   cluster_group       = "dev-sandbox"
   entity              = "org1"
--- terraform/dev/eks.tf
+++ terraform/dev/eks.tf
@@ -1,14 +1,4 @@
 # ---------------------------------------------------------------------------------------------------------------------
-# CloudWatch - log group
-# ---------------------------------------------------------------------------------------------------------------------
-
-resource "aws_cloudwatch_log_group" "eks_cluster" {
-  name              = "/aws/eks/${var.cluster_name}/cluster"
-  kms_key_id        = aws_kms_key.eks-kms_key.key_id
-  retention_in_days = 1
-}
-
-# ---------------------------------------------------------------------------------------------------------------------
 # VPC
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -28,13 +18,13 @@ module "vpc" {
   enable_dns_hostnames = true
 
   public_subnet_tags = {
-    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                    = 1
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"           = 1
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
   }
 }
 
@@ -49,6 +39,70 @@ resource "aws_kms_key" "eks-kms_key" {
 }
 
 resource "aws_kms_alias" "eks" {
-  name          = "alias/${var.cluster_name}"
+  name          = "alias/${local.cluster_name}"
   target_key_id = aws_kms_key.eks-kms_key.key_id
 }
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Create the EKS cluster
+# ---------------------------------------------------------------------------------------------------------------------
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "18.7.2"
+
+  cluster_name                    = local.cluster_name
+  cluster_version                 = var.cluster_version
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  cluster_encryption_config = [{
+    provider_key_arn = aws_kms_key.eks-kms_key.arn
+    resources        = ["secrets"]
+  }]
+
+  cluster_addons = {
+    coredns = {
+      addon_version     = "v1.8.4-eksbuild.1"
+      resolve_conflicts = "OVERWRITE"
+    }
+    kube-proxy = {
+      addon_version     = "v1.21.2-eksbuild.2"
+      resolve_conflicts = "OVERWRITE"
+    }
+    vpc-cni = {
+      addon_version     = "v1.10.2-eksbuild.1"
+      resolve_conflicts = "OVERWRITE"
+    }
+    aws-ebs-csi-driver = {
+      addon_version     = "v1.4.0-eksbuild.preview"
+      resolve_conflicts = "OVERWRITE"
+    }
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  cloudwatch_log_group_retention_in_days = var.cloudwatch_log_group_retention_in_days
+  cluster_enabled_log_types              = var.cluster_enabled_log_types
+
+  eks_managed_node_group_defaults = {
+    iam_role_additional_policies = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
+  }
+
+  eks_managed_node_groups = var.eks_managed_node_groups
+
+  tags = merge(local.aws_default_tags, { Name = local.cluster_name })
+}
+
+data "aws_eks_cluster" "eks-cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "eks-cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_caller_identity" "current" {}
