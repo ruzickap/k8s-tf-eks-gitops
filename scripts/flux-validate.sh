ac4d08a664c26a1044f9b896dc6ae3d8216d4ac1
Petr Ruzicka <petr.ruzicka@gmail.com>
2022-05-01 06:49:03 +0200

    Add parameters to Flux apps

--- scripts/flux-validate.sh
+++ scripts/flux-validate.sh
@@ -54,14 +54,14 @@ find ./clusters -maxdepth 2 -type f -name '*.yaml' -print0 | while IFS= read -r 
 done
 
 # mirror kustomize-controller build options
-kustomize_flags="--load-restrictor=LoadRestrictionsNone"
+kustomize_flags=("--load-restrictor=LoadRestrictionsNone")
 kustomize_config="kustomization.yaml"
 
 echo "INFO - Validating kustomize overlays"
 find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
   do
     echo "INFO - Validating kustomization ${file/%$kustomize_config}"
-    kustomize build "${file/%$kustomize_config}" "$kustomize_flags" | \
+    kustomize build "${file/%$kustomize_config}" "${kustomize_flags[@]}" | \
       kubeconform "${kubeconform_config[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
